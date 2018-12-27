1a3fb1a426e8773d485894127579a07d38745d37
kari.kolehmainen@vtt.fi <kari.kolehmainen@abysscore.com>
2018-12-27 22:40:01 +0200

    asdf

--- MessageBroker.py
+++ MessageBroker.py
@@ -6,6 +6,15 @@ from Camera import *
 def on_log(client, userdata, level, buf):
 	print("log: ",buf)
 
+def on_message(client, userdata, message):
+	msg =str(message.payload.decode("utf-8"))
+	print("message received " ,msg)
+	print("message topic=",message.topic)
+	print("message qos=",message.qos)
+	print("message retain flag=",message.retain)
+#	if (msg == "start"):
+#		self.camera.running = 1
+
 class MessageBroker:
 	mqtturl = "null"
 	uname = "name"
@@ -19,7 +28,7 @@ class MessageBroker:
 		self.passwd=password
 		self.camera = camera
 		self.client=mqtt.Client("CameraDolly")
-		self.client.on_message=self.on_message
+		self.client.on_message=on_message
 		self.client.on_log=on_log
 		print("DataTransmitter.Init ready")
 		#def __del__(self):
