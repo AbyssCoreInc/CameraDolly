970c88638b91d614b3a73bbafce0ecc36e9ed39a
kari.kolehmainen@vtt.fi <kari.kolehmainen@abysscore.com>
2018-12-27 19:53:56 +0200

    asd

---
+++ Configuration.py
@@ -0,0 +1,31 @@
+import json
+from pprint import pprint
+
+class Configuration:
+	json_conf = []
+	def __intt__(self):
+		print("Init")
+	
+	def readConfiguration(self):
+		print("Reading configuration file")
+		with open('/etc/cameradolly.json', 'r') as handle:
+			self.json_conf = json.load(handle)
+	def getServerURL(self):
+		return self.json_conf['configuration']['server_address']
+	def getMqttUsername(self):
+		return self.json_conf["configuration"]["mqtt_username"]
+	def getMqttPassword(self):
+		return self.json_conf["configuration"]["mqtt_password"]
+	def getMQTTURL(self):
+		return self.json_conf["configuration"]["mqtt_address"]
+	def getTopic(self):
+		return self.json_conf["configuration"]["mqtt_topic"]
+	def getStepsPerFrame(self):
+		return self.json_conf["configuration"]["stepsperframe"]
+	def getDefaultDirection(self):
+		return self.json_conf["configuration"]["defaultdirection"]
+	def getDefaultImages(self):
+		return self.json_conf["configuration"]["defaultimages"]
+	def getStepperSpeed(self):
+		return self.json_conf["configuration"]["stepperspeed"]
+
