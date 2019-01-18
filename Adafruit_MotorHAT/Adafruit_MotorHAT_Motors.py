be9566aa83eac3bbf8e6a56d714009ef127c3797
kari.kolehmainen@vtt.fi <kari.kolehmainen@abysscore.com>
2019-01-18 11:14:42 +0200

    streamlined stepper files

--- Adafruit_MotorHAT/Adafruit_MotorHAT_Motors.py
+++ Adafruit_MotorHAT/Adafruit_MotorHAT_Motors.py
@@ -72,23 +72,9 @@ class Adafruit_StepperMotor:
                     self.currentstep += self.MICROSTEPS
                 else:
                     self.currentstep -= self.MICROSTEPS
-        if (style == Adafruit_MotorHAT.INTERLEAVE):
-            if (dir == Adafruit_MotorHAT.FORWARD):
-                self.currentstep += self.MICROSTEPS//2
-            else:
-                self.currentstep -= self.MICROSTEPS//2
-
-        if (style == Adafruit_MotorHAT.MICROSTEP):
-            if (dir == Adafruit_MotorHAT.FORWARD):
-                self.currentstep += 1
-            else:
-                self.currentstep -= 1
-
-                # go to next 'step' and wrap around
-                self.currentstep += self.MICROSTEPS * 4
-                self.currentstep %= self.MICROSTEPS * 4
 
             pwm_a = pwm_b = 0
+			
             if (self.currentstep >= 0) and (self.currentstep < self.MICROSTEPS):
                 pwm_a = self.MICROSTEP_CURVE[self.MICROSTEPS - self.currentstep]
                 pwm_b = self.MICROSTEP_CURVE[self.currentstep]
@@ -145,72 +131,10 @@ class Adafruit_StepperMotor:
     def step(self, steps, direction, stepstyle):
         s_per_s = self.sec_per_step
         lateststep = 0
-
-        if (stepstyle == Adafruit_MotorHAT.INTERLEAVE):
-            s_per_s = s_per_s / 2.0
-        if (stepstyle == Adafruit_MotorHAT.MICROSTEP):
-            s_per_s /= self.MICROSTEPS
-            steps *= self.MICROSTEPS
-
         print("{} sec per step".format(s_per_s))
-
         for s in range(steps):
             lateststep = self.oneStep(direction, stepstyle)
             time.sleep(s_per_s)
-
-        if (stepstyle == Adafruit_MotorHAT.MICROSTEP):
-            # this is an edge case, if we are in between full steps, lets just keep going
-            # so we end on a full step
-            while (lateststep != 0) and (lateststep != self.MICROSTEPS):
-                lateststep = self.oneStep(direction, stepstyle)
-                time.sleep(s_per_s)
-
-class Adafruit_DCMotor:
-    def __init__(self, controller, num):
-        self.MC = controller
-        self.motornum = num
-        pwm = in1 = in2 = 0
-
-        if (num == 0):
-                 pwm = 8
-                 in2 = 9
-                 in1 = 10
-        elif (num == 1):
-                 pwm = 13
-                 in2 = 12
-                 in1 = 11
-        elif (num == 2):
-                 pwm = 2
-                 in2 = 3
-                 in1 = 4
-        elif (num == 3):
-                 pwm = 7
-                 in2 = 6
-                 in1 = 5
-        else:
-            raise NameError('MotorHAT Motor must be between 1 and 4 inclusive')
-        self.PWMpin = pwm
-        self.IN1pin = in1
-        self.IN2pin = in2
-
-    def run(self, command):
-        if not self.MC:
-            return
-        if (command == Adafruit_MotorHAT.FORWARD):
-            self.MC.setPin(self.IN2pin, 0)
-            self.MC.setPin(self.IN1pin, 1)
-        if (command == Adafruit_MotorHAT.BACKWARD):
-            self.MC.setPin(self.IN1pin, 0)
-            self.MC.setPin(self.IN2pin, 1)
-        if (command == Adafruit_MotorHAT.RELEASE):
-            self.MC.setPin(self.IN1pin, 0)
-            self.MC.setPin(self.IN2pin, 0)
-    def setSpeed(self, speed):
-        if (speed < 0):
-            speed = 0
-        if (speed > 255):
-            speed = 255
-        self.MC._pwm.setPWM(self.PWMpin, 0, speed*16)
 
 
 class Adafruit_MotorHAT:
@@ -221,12 +145,9 @@ class Adafruit_MotorHAT:
 
     SINGLE = 1
     DOUBLE = 2
-    INTERLEAVE = 3
-    MICROSTEP = 4
 
     def __init__(self, addr = 0x66, freq = 1600, i2c=None, i2c_bus=None):
         self._frequency = freq
-        self.motors = [ Adafruit_DCMotor(self, m) for m in range(4) ]
         self.steppers = [ Adafruit_StepperMotor(self, 1), Adafruit_StepperMotor(self, 2) ]
         self._pwm = PWM(addr, debug=False, i2c=i2c, i2c_bus=i2c_bus)
         self._pwm.setPWMFreq(self._frequency)
@@ -246,7 +167,3 @@ class Adafruit_MotorHAT:
             raise NameError('MotorHAT Stepper must be between 1 and 2 inclusive')
         return self.steppers[num-1]
 
-    def getMotor(self, num):
-        if (num < 1) or (num > 4):
-            raise NameError('MotorHAT Motor must be between 1 and 4 inclusive')
-        return self.motors[num-1]
