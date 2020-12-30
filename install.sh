eb9cbf8a65223270e862c0fd7924a9ea3c7a37f0
kari.kolehmainen@vtt.fi <kari.kolehmainen@abysscore.com>
2020-12-30 14:42:51 +0200

    install sync

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 apt-get -y install gphoto2
 apt-get -y install libgphoto2-dev
-
+apt-get -y install mosquitto
+apt-get -y install mosquitto-clients
 
 pip3 install gphoto2
 pip3 install paho-mqtt
@@ -13,4 +14,5 @@ pip3 install pytz
 cp cameradolly.json /etc/
 cp cameradolly.service /etc/systemd/system/
 systemctl deamon-reload
+systemctl start mosquitto
 systemctl start cameradolly
